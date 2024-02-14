28ad9da81eef5dfc3cec5709c3d5ccc84113dd24
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2024-02-14 15:53:07 +0300

    Login

---
+++ DTO/LoginDTO.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DTO
+{
+
+   public class LoginDTO
+   {
+      [Required]
+      public string Email { get; set; } = null!;
+
+      [Required]
+      public string Password { get; set; } = null!;
+
+
+   }
+
+
+}
--- obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b188725c02e0ca1b718916ae84eebb14e159042f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3bdd05fc8dcbcd3a14346a2e1ef9659f296eb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
